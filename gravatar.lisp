87fcb67bf4ee577e6e912af9a5e404a0a5e8cf38
Ury Marshak <ury@feline.local>
2013-11-25 16:03:01 +0200

    fix parameters for the new Drakhma

--- gravatar.lisp
+++ gravatar.lisp
@@ -24,7 +24,8 @@
     (puri:merge-uris (format nil "avatar/~a~@[?~a~]"
                              (hash email)
                              (drakma::alist-to-url-encoded-string parameters
-                                                                  :utf-8))
+                                                                  :utf-8
+                                                                  #'drakma:url-encode))
                      +base-uri+)))
 
 (defun generate-profile-url (email type parameters)
